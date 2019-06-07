b1daf69585ffe2766c3d258a5456c14be2da5214
Justin Needham <justin.needham@alumnimail.pepperdine.edu>
2019-06-07 14:59:46 -0600

    dynamic content

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import Link from 'next/Link';
 import Layout from '../components/Layout';
+
+const PostLink = props => (
+    <li>
+        <Link href={`/post?title=${props.title}`}>
+            <a>{props.title}</a>
+        </Link>
+    </li>
+)
 
 const Index = () => (
     <div>
         <Layout>
-            <p>Hello Next.js</p>
+            <h1>My Blog</h1>
+            <ul>
+                <PostLink title="Hello Next.js"/>
+                <PostLink title="Learn Next.js is awesome"/>
+                <PostLink title="Deploy apps with Zeit"/>
+            </ul>
         </Layout>
     </div>
 );
